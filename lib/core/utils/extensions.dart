dda089ccf0f2c9e27c6a89bd742f9bb38a8f48de
Pablo Stefan Gomes Pereira <pablo.stefan.dev@gmail.com>
2025-01-04 13:43:41 -0300

    Remove adaptive layout and add Firebase core integration.
    Deleted the `adaptive.dart` file and replaced redundant email validation logic with the `email_validator` package. Integrated Firebase by adding its dependencies into `pubspec.yaml` and initializing it in `main.dart`.

--- lib/core/utils/extensions.dart
+++ lib/core/utils/extensions.dart
@@ -1,7 +1,5 @@
-extension EmailValidator on String {
-  bool get isValidEmail {
-    return RegExp(
-            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
-        .hasMatch(this);
-  }
+import 'package:email_validator/email_validator.dart';
+
+extension Validator on String {
+  bool get isValidEmail => EmailValidator.validate(this);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:portfolio/app/app_widget.dart';
+import 'package:portfolio/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setUrlStrategy(PathUrlStrategy());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(Portfolio());
 }
